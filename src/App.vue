162fe7fbe1b9186f4b770edebc9f293f1507a777
ArunaTebel <arunatebel@hotmail.com>
2022-07-24 17:30:22 +0530

    Adding the yarn.lock file

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,28 @@
 <template>
-  <h1 class="text-2xl font-bold underline">
-    Hello world!
-  </h1>
-  <div>
-    <button v-if="!isAuthenticated" @click="login">Log in</button>
-    <button v-if="isAuthenticated" @click="logout">Log out</button>
-    <pre v-if="isAuthenticated"><code>{{ user }}</code></pre>
-    <button v-if="isAuthenticated" @click="showAccessToken">Show Access Token</button>
-    <pre><code id="access-token"></code></pre>
-  </div>
+  <TopNavBar/>
+  <input placeholder="Channel Name" v-model="channelName">
+  <input placeholder="Channel Description" v-model="channelDescription">
+  <button @click="addChannel">Add Channel</button>
 </template>
 <script>
-import {useAuth0} from '@auth0/auth0-vue';
+import TopNavBar from "./components/layouts/TopNavBar.vue";
+import WS from "./lib/WebService";
 
 export default {
-  setup() {
-    const {loginWithRedirect, logout, isAuthenticated, user, getAccessTokenSilently} = useAuth0()
-
+  components: {TopNavBar},
+  data: () => {
     return {
-      login: () => {
-        loginWithRedirect();
-      },
-      logout: () => {
-        logout({returnTo: window.location.origin})
-      },
-      user,
-      isAuthenticated,
-      showAccessToken: async () => {
-        const accessToken = await getAccessTokenSilently()
-        document.getElementById('access-token').innerHTML = accessToken
-        localStorage.setItem('access-token', accessToken);
-      }
-    };
+      channelName: '',
+      channelDescription: '',
+    }
+  },
+  methods: {
+    addChannel: function () {
+      WS.models.channels.create({
+        name: this.channelName,
+        description: this.channelDescription
+      });
+    }
   }
 };
 </script>
